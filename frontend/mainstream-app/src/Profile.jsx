38541a411c2f24328a9ee03ffc2d629ef3d438a7
Tessa Orozco <tessaoro@meta.com>
2024-07-03 11:59:48 -0700

    user can search music and add to their playlist

--- frontend/mainstream-app/src/Profile.jsx
+++ frontend/mainstream-app/src/Profile.jsx
@@ -6,63 +6,86 @@ import Playlist from "./Playlist";
 function Profile( {curUser} ) {
     const [searchQuery, setSearchQuery] = useState("");
     const [searchResult, setSearchResult] = useState([]);
-    const [playerToAdd, setPlayerToAdd] = useState("");
     const [toAddTitle, setToAddTitle] = useState("addLater");
     const [result, setResult] = useState("");
+    const [songAdded, setSongAdded] = useState("");
 
     useEffect(() => {
         fetchSearch();
     }, [searchQuery]);
 
+    /*
+    * Fetches youtube search results that match search query
+    */
     const fetchSearch = () => {
-        let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&maxResults=3&type=video&videoCategoryId=10&key=${import.meta.env.VITE_API_KEY}`;
+        if (searchQuery.trim().length !== 0) {
+            let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&maxResults=3&type=video&videoCategoryId=10&key=${import.meta.env.VITE_API_KEY}`;
+            fetch(url)
+                .then(response => response.json())
+                .then(response => {setSearchResult(response.items);
+                })
+                .catch(err => {});
+        } else {
+            setSearchResult([]);
+        }
+    }
+
+    /*
+    * Sets search query to the inputted title and artist 
+    */
+    const handleSearch = (e) => {
+        e.preventDefault();
+        setSearchQuery(e.target.elements.searchQ.value + e.target.elements.searchA.value);
+    }
+
+    /*
+    * Fetches video info for the selected song and sets the embed html to add to database
+    */
+    const fetchSong = (vidID) => {
+        let url = `https://www.googleapis.com/youtube/v3/videos?part=player&id=${vidID}&key=${import.meta.env.VITE_API_KEY}`;
         fetch(url)
             .then(response => response.json())
-            .then(response => {setSearchResult(response.items);
+            .then(response => {
+                addSongToUser(response.items[0].player.embedHtml);
+                setSongAdded(response.items[0].player.embedHtml);
             })
             .catch(err => {});
     }
 
-    const handleSearch = (e) => {
-        e.preventDefault();
-        setSearchQuery(e.target.elements.searchQ.value + " " + e.target.elements.searchA.value);
+    /*
+    * Adds the selected song to the user playlist in database
+    */
+    const addSongToUser = ( playerToAdd ) => {
+        fetch(`${import.meta.env.VITE_BACKEND_ADDRESS}/songs/${curUser}/create/`,
+        {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: toAddTitle,
+                player: playerToAdd
+            }),
+        })
+        .then(response => {
+            if (response.ok) {
+                setResult("added to playlist!");
+            } else {
+                setResult("failed to add song");
+            }
+        })
+        .catch(error => {
+            setResult("failed to add song");
+        });
     }
 
-    // const fetchSong = (vidID) => {
-    //     let url = `https://www.googleapis.com/youtube/v3/videos?part=player&id=${vidID}&key=${import.meta.env.VITE_API_KEY}`;
-    //     fetch(url)
-    //         .then(response => response.json())
-    //         .then(response => {
-    //             setPlayerToAdd(response.items[0].player.embedHtml);
-    //         })
-    //         .catch(err => {});
-    //     addSongToUser();
-    // }
-
-    // const addSongToUser = () => {
-    //     fetch(`${import.meta.env.VITE_BACKEND_ADDRESS}/songs/${curUser}/create/`,
-    //     {
-    //         method: 'POST',
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //         body: JSON.stringify({
-    //             toAddTitle,
-    //             playerToAdd
-    //         }),
-    //     })
-    //     .then(response => {
-    //         if (response.ok) {
-    //             setResult("added song!");
-    //         } else {
-    //             setResult("failed to add song");
-    //         }
-    //     })
-    //     .catch(error => {
-    //         setResult("failed to add song");
-    //     });
-    // }
-
+    /*
+    * Clears search query variable
+    */
+    const clearSearch = () => {
+        setSearchQuery("");
+        setResult("");
+    }
 
     return (
       <div id="profileContainer">
@@ -75,12 +98,13 @@ function Profile( {curUser} ) {
             <input name="searchA" required></input>
             <button type="submit" value="Submit">Go</button>
         </form>
+        <button onClick={() => clearSearch()}>Clear search</button>
         {searchResult && <div>
             {searchResult.map((searchResult, index) => (
-                <p /*onClick={fetchSong(searchResult.id.videoId)}*/>{searchResult.snippet.title}</p>)                          
+                <p id="searchResult" onClick={() => fetchSong(searchResult.id.videoId)}>{searchResult.snippet.title}</p>)                          
             )}
         </div>}
-        <Playlist curUser={curUser}></Playlist>
+        <Playlist curUser={curUser} songAdded={songAdded}></Playlist>
       </div>
     )
 }
