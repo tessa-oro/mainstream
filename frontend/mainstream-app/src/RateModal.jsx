4a790b8f1c8f3d9fed3a89782461e1aeb8ae8e3c
Tessa Orozco <tessaoro@meta.com>
2024-07-05 17:34:21 -0700

    score gets added to song ratings when user submits number

--- frontend/mainstream-app/src/RateModal.jsx
+++ frontend/mainstream-app/src/RateModal.jsx
@@ -3,27 +3,61 @@ import React from "react";
 import { useState, useEffect } from 'react';
 
 const RateModal = ({ songID, closeModal}) => {
+    const [num, setNum] = useState("");
+
+    const handleRate = (e) => {
+        e.preventDefault();
+        fetch(`${import.meta.env.VITE_BACKEND_ADDRESS}/song/rate/${songID}/${num}`,
+            {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+        .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                } else {
+                return response.json();
+                } 
+        })
+        .then(data => {
+            console.log(data);
+            closeModal();
+        })
+        .catch(error => {
+            console.error('Error fetching post', error);
+        });
+    }
+
+    const pickNum = (e) => {
+        e.preventDefault();
+        setNum(e.target.value);
+    }
 
     return (
         <>
-            <div id="followModal">
+            <div id="rateModal">
                 <div id="modalContent">
                     <h2>Rate song</h2>
-                    <form id="rateForm">
+                    <form id="rateForm" onSubmit={(e) => handleRate(e)}>
                         <div id="rateButtons">
-                                <button>1</button>
-                                <button>2</button>
-                                <button>3</button>
-                                <button>4</button>
-                                <button>5</button>
-                                <button>6</button>
-                                <button>7</button>
-                                <button>8</button>
-                                <button>9</button>
-                                <button>10</button>
+                                <button type="button" value="1" onClick={(e) => pickNum(e)}>1</button>
+                                <button type="button" value="2" onClick={(e) => pickNum(e)}>2</button>
+                                <button type="button" value="3" onClick={(e) => pickNum(e)}>3</button>
+                                <button type="button" value="4" onClick={(e) => pickNum(e)}>4</button>
+                                <button type="button" value="5" onClick={(e) => pickNum(e)}>5</button>
+                                <button type="button" value="6" onClick={(e) => pickNum(e)}>6</button>
+                                <button type="button" value="7" onClick={(e) => pickNum(e)}>7</button>
+                                <button type="button" value="8" onClick={(e) => pickNum(e)}>8</button>
+                                <button type="button" value="9" onClick={(e) => pickNum(e)}>9</button>
+                                <button type="button" value="10" onClick={(e) => pickNum(e)}>10</button>
                         </div>
+                        {num && <p id="selectedNum">{num}</p>}
                         <input type="checkbox"></input>
                         <label>I already know this song</label>
+                        <button id="submit" type="submit">Submit</button>
                    </form>
                 </div>
                 <button id="close" onClick={closeModal}>cancel</button>
