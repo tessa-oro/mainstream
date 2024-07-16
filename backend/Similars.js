45c964d477c7b00ea58eab3fad7f3e11b44cc7e9
Tessa Orozco <tessaoro@meta.com>
2024-07-15 17:29:21 -0700

    calculate pearson coefficient

--- backend/Similars.js
+++ backend/Similars.js
@@ -3,17 +3,20 @@ const prisma = new PrismaClient();
 
 class Similars {
 
+    /*
+    * Updates the set of similar users based on song ratings
+    */
     async update(user) {
         try {
-            const userRatings = await prisma.interactions.findMany({
+            const userRatings = await prisma.interactions.findMany({ //get all the rating occurrences a user has made
                 where: { user: user }
             });
-            const userSongs = userRatings.map(rating => rating.songItem);
-            const allRatings = await prisma.interactions.findMany({
+            const userSongs = userRatings.map(rating => rating.songItem); //get all the songs a user had rated
+            const allRatings = await prisma.interactions.findMany({ //get all the rating occurrences made by other users for the songs a user has rated
                 where: {songItem: { in: userSongs }, NOT: { user }}
             });
             const others = {};
-            allRatings.forEach(interaction => {
+            allRatings.forEach(interaction => { //map the rating for all rating occurrences to the user who made the rating
                 if (!others[interaction.user]) {
                     others[interaction.user] = [];
                 }
@@ -21,7 +24,7 @@ class Similars {
             });
             const similarities = {};
             for (const [otherUser, ratings] of Object.entries(others)) {
-                similarities[otherUser] = this.computePearson(userSongs, ratings);
+                similarities[otherUser] = this.computePearson(userRatings, ratings);
             }
             await prisma.user.update({
                 where: { user: user },
@@ -33,7 +36,34 @@ class Similars {
         }
     }
 
+    /*
+    * Computes the Pearson Correlation Coefficient for two users based on their ratings of songs
+    */
     async computePearson(userRatings, otherRatings) {
-
+        const common = {};
+        let n = 0; //number of common ratings
+        userRatings.forEach(rating => {
+            otherRatings.forEach(otherRating => {
+                if (rating.songItem == otherRating.songItem) {
+                    common[rating.songItem] = { user: rating.rating, other: otherRating.rating}
+                    n += 1;
+                }
+            })
+        })
+        let sumR = 0; //sum of user ratings
+        let sumOR = 0; //sum of other user ratings
+        let sumRsq = 0; //sum of squared user ratings
+        let sumORsq = 0; //sum of squared other user ratings
+        let sumCrossP = 0; //sum of cross products
+        common.forEach((ratings, _) => {
+            sumR += ratings.user;
+            sumOR += ratings.other;
+            sumRsq += ratings.user * ratings.user;
+            sumORsq += ratings.other * ratings.other;
+            sumCrossP += ratings.user * ratings.other;
+        })
+        let numerator = (n*sumCrossP)-(sumR * sumOR)
+        let denominator = Math.sqrt((n*sumRsq)-(sumR*sumR))((n*sumORsq)-(sumOR*sumOR))
+        return numerator / denominator;
     }
 }
