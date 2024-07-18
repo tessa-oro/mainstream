475afae545641b0e20c17336b0071a55a09984ed
Tessa Orozco <tessaoro@meta.com>
2024-07-18 15:10:06 -0700

    using apis to get video transcript and text sentiment analysis

--- backend/UserAnalysis.js
+++ backend/UserAnalysis.js
@@ -2,9 +2,34 @@ class UserAnalysis {
 
     constructor(prisma) {
         this.prisma = prisma;
+        this.getTranscript(videoId) = require('youtube-transcript-api');
     }
 
-    //for each song in playlist get the statistics
+    async textapi(inputText) {
+        const fetch = (await import('node-fetch')).default;
+        const FormData = require('form-data');
+    
+        const data = new FormData();
+        data.append('text', inputText);
+        data.append('language', 'english');
+    
+        const url = 'https://japerk-text-processing.p.rapidapi.com/sentiment/';
+        const options = {
+        method: 'POST',
+        headers: {
+            'x-rapidapi-key': '60caffb22cmsh4549d89b2643826p19684djsncc8300b7fec4',
+            'x-rapidapi-host': 'japerk-text-processing.p.rapidapi.com',
+            ...data.getHeaders(),
+        },
+        body: data
+        };
+    
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            return result;
+        } catch (error) {}
+    }
 
 }
 
