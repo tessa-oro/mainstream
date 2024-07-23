25416890101c8b3c2098f027a5b4437f349ee1cc
Tessa Orozco <tessaoro@meta.com>
2024-07-22 17:04:29 -0700

    create fetch for displaying user top emotion in analysis modal

--- frontend/mainstream-app/src/UserAnalysisModal.jsx
+++ frontend/mainstream-app/src/UserAnalysisModal.jsx
@@ -4,11 +4,16 @@ import { useState, useEffect } from 'react';
 
 const UserAnalysisModal = ({ closeModal, curUser }) => {
     const [analysisResult, setAnalysisResult] = useState("");
+    const [topEmotion, setTopEmotion] = useState("");
 
     useEffect(() => {
         fetchUserAnalysis();
+        fetchTopEmotion();
     }, [curUser]);
 
+    /*
+    * Fetch a user's playlist analysis using GET
+    */
     const fetchUserAnalysis = () => {
         fetch(`${import.meta.env.VITE_BACKEND_ADDRESS}/userAnalysis/${curUser}`)
         .then(response => response.json())
@@ -16,7 +21,21 @@ const UserAnalysisModal = ({ closeModal, curUser }) => {
             setAnalysisResult(data);
         })
         .catch(error => {
-            setAnalysisResult("no analysis to show");
+            setAnalysisResult("No analysis to show");
+        });
+    }
+
+    /*
+    * Fetch a user's top emotion using GET
+    */
+    const fetchTopEmotion = () => {
+        fetch(`${import.meta.env.VITE_BACKEND_ADDRESS}/topEmotion/${curUser}`)
+        .then(response => response.json())
+        .then(data => {
+            setTopEmotion("The most prevalent emotion in your playlist is: " + JSON.stringify(data).replace(/["]/g, ''));
+        })
+        .catch(error => {
+            setTopEmotion("Cannot get top emotion");
         });
     }
 
@@ -25,6 +44,7 @@ const UserAnalysisModal = ({ closeModal, curUser }) => {
             <div id="analysisModal">
                 <div id="modalContent">
                     {analysisResult && <p>{analysisResult}</p>}
+                    {topEmotion && <p>{topEmotion}</p>}
                 </div>
                 <button id="close" onClick={closeModal}>cancel</button>
             </div>
